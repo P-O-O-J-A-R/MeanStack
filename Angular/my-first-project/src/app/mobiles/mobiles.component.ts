ed7dd8197e2b83f0c7ae7077fd013d4d8a68c800
P-O-O-J-A-R <r.pooja9586@gmail.com>
2023-11-04 20:44:58 +0530

    Services & Observables
    Signed-off-by: P-O-O-J-A-R <r.pooja9586@gmail.com>

--- Angular/my-first-project/src/app/mobiles/mobiles.component.ts
+++ Angular/my-first-project/src/app/mobiles/mobiles.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-mobiles',
   templateUrl: './mobiles.component.html',
   styleUrls: ['./mobiles.component.css']
 })
-export class MobilesComponent {
+export class MobilesComponent implements OnInit{
 
+  ngOnInit(): void {
+    this.obj.getProducts().subscribe({
+      next:data=>console.log(data),
+      error:err=>console.log(err),
+      complete:()=>console.log('Complete')
+    })
+  }
+  data!:any;
+  constructor(private obj:DataService){}
+  addItem(){
+    this.data = this.obj.getData();
+    console.log(this.data);
+  }
 }
